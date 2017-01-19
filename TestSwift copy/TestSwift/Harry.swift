e71a8a51d8ec78c98dcb9a77b66ba44d70b76e3b
Jesse Sahli <jessesahli3@gmail.com>
2017-01-19 12:36:09 -0500

    harry local

---
+++ TestSwift copy/TestSwift/Harry.swift
@@ -0,0 +1,21 @@
+//
+//  Harry.swift
+//  TestSwift
+//
+//  Created by Jesse Sahli on 1/19/17.
+//  Copyright © 2017 sahlitude. All rights reserved.
+//
+
+import UIKit
+
+class Harry: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TestSwift copy/TestSwift/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TestSwift
+//
+//  Created by Jesse Sahli on 1/19/17.
+//  Copyright © 2017 sahlitude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // code by tom
+        
+        
+        
+        // added by harry
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
