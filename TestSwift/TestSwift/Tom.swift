9819f01a4122c9bbaff5fbefaf9f2fd135d90576
Jesse Sahli <jessesahli3@gmail.com>
2017-01-19 12:27:25 -0500

    added tom.swift by tom

---
+++ TestSwift/TestSwift/Tom.swift
@@ -0,0 +1,21 @@
+//
+//  Tom.swift
+//  TestSwift
+//
+//  Created by Jesse Sahli on 1/19/17.
+//  Copyright © 2017 sahlitude. All rights reserved.
+//
+
+import UIKit
+
+class Tom: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
